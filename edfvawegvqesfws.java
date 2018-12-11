566d3c59c5659f72248efc41ca3f104bdea54cb2
Oliver-Finlay-CS <o.finlay@share.epsb.ca>
2018-12-11 12:19:45 -0700

    srdtgfhggfsddf

--- edfvawegvqesfws.java
+++ edfvawegvqesfws.java
@@ -1,4 +1,34 @@
-public class edfvawegvqesfws
+/**<h1>drghbexdfbdtrgbh </h1>
+*<b>Project Purpose</b><br>
+*<p>erbvwe]f[kpnow4eprkotbuw]</p>
+*<b>InstructionsForGeniuses</b><br>
+*<p>dfuowerniwebuonwerwprmo</p>
+*<b>User Instructions:<b/><br>
+*<p>iargnoatrtra</P>
+*<br>
+*@author Ugandan Warrior
+*@version 1
+*@since 2017-11-15
+*/
+
+public class edfvawegvqesfws {
+  /**<b>Class Description: Necessary Class</b><br>
+  *<p>vyuk rkvyujvtbil7tli7tui6rvuytr78ouytbuituitui</p>
+  *@param
+  *@return
+  *@throws
+  */
+
+  /**<b>Method Description: Main, Driver</b><br>
+  *<p>pljgdrfserfgyhgvyhu</p>
+  *@param args unused
+  *@return
+  *@throws
+  */
+   }
+}
+
+
 {
   public static void main (String[] args)
   {
